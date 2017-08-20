bc15f4885a6906c3bf1b40c2eab74e361148265c
Winston Chang <winston@stdout.org>
2017-08-20 13:52:10 -0500

    Build full book with bookdown

---
+++ utils.R
@@ -0,0 +1,22 @@
+knitr::opts_chunk$set(collapse = TRUE, comment = "#>", cache = TRUE)
+library(ggplot2)
+
+knit_print.data.frame <- function(x, ..., maxrows = 8) {
+  output <- capture.output(
+    base::print.data.frame(head(x, maxrows), ...)
+  )
+  
+  if (nrow(x) > maxrows) {
+    output <- c(
+      output,
+      paste(
+        "# ... with",
+        format(nrow(x) - maxrows, big.mark = ","),
+        "more rows"
+      )
+    )
+  }
+
+  cat(output, sep = "\n")
+}
+
