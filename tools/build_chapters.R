32cf2c80ca3726033e3434366efb67e0603e4756
Winston Chang <winston@stdout.org>
2017-10-16 21:20:45 -0500

    Put chapters in separate dirs

--- tools/build_chapters.R
+++ tools/build_chapters.R
@@ -11,12 +11,18 @@ files <- commandArgs(trailingOnly = TRUE)
 if (!dir.exists("chapters_md"))
   dir.create("chapters_md")
 
-setwd("chapters_md")
-
 for (file in files) {
-  file_md <- sub("\\.Rmd$", ".md", file)
-  knitr::knit(
-    file.path("..", file),
-    output = file_md
-  )
+  file_no_ext <- sub("\\.Rmd$", "", file)
+  file_md <- paste0(file_no_ext, ".md")
+
+  dest_dir <- file.path("chapters_md", file_no_ext)
+  if (!dir.exists(dest_dir))
+    dir.create(dest_dir, recursive = TRUE)
+
+  withr::with_dir(dest_dir, {
+    knitr::knit(
+      file.path("..", "..", file),
+      output = file_md
+    )
+  })
 }
